147ffdcb000f5e2e29b83992359eaad965027655
roushanzameer6009 <143252855+roushanzameer6009@users.noreply.github.com>
2025-01-27 07:07:51 -0800

    Update info.py

--- info.py
+++ info.py
@@ -25,9 +25,7 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/LiveBollywood4k
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002265690150').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://roushanzameer6009:roushanzameer6009@livebollywood4kmovies.m70ee.mongodb.net/?retryWrites=true&w=majority&appName=LiveBollywood4kMovies
-LOG_CHANNEL:-1002448176597
-")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://roushanzameer6009:roushanzameer6009@livebollywood4kmovies.m70ee.mongodb.net/?retryWrites=true&w=majority&appName=LiveBollywood4kMovies")
 DATABASE_NAME = environ.get('DATABASE_NAME', "LiveBollywood4kMovies")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
