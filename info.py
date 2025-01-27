eaef7ed5d101a3dbb80df901ae91cc5f84d65e6d
roushanzameer6009 <143252855+roushanzameer6009@users.noreply.github.com>
2025-01-27 07:45:39 -0800

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ import re
 from os import environ,getenv
 from Script import script
 
-id_pattern = re.compile(r'^.\d+$')
+id_pattern = re.compile(re'^.\d+$')
 def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
         return True
